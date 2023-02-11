00df5e04519e8949b5965f0786f8a079b9968be3
AL JUBAIAR <66907963+aljubaiarde@users.noreply.github.com>
2023-02-11 23:25:42 +0100

    Create restrict.php

---
+++ restrict.php
@@ -0,0 +1,10 @@
+<?php
+
+$allowed_ips = array('111.111.111.111', '222.222.222.222', '333.333.333.333', '444.444.444.444');
+
+if (!in_array($_SERVER['REMOTE_ADDR'], $allowed_ips)) {
+    header('HTTP/1.0 403 Forbidden');
+    exit;
+}
+
+?>
