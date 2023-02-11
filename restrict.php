e2e268a09f64a5d66c0f9c0218d277dd8f62496b
AL JUBAIAR <66907963+aljubaiarde@users.noreply.github.com>
2023-02-11 23:34:02 +0100

    Update restrict.php

--- restrict.php
+++ restrict.php
@@ -4,6 +4,7 @@ $allowed_ips = array('111.111.111.111', '222.222.222.222', '333.333.333.333', '4
 
 if (!in_array($_SERVER['REMOTE_ADDR'], $allowed_ips)) {
     header('HTTP/1.0 403 Forbidden');
+    echo 'Access is restricted to specific IP addresses only.';
     exit;
 }
 
